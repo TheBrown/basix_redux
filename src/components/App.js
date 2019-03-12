d913c3f00d0757cb17653b7dd98922da7d035afc
saleumsack keoboualay <saleumsack.bcel@gmail.com>
2019-03-12 14:05:20 +0700

    Add Bulma css and design App.js presentational component

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
+import { connect } from 'react-redux';
+
+const App = ({ counter, message }) => {
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          </a>
+        <h1>Counter: {counter}</h1>
+        <br/>
+        <div>
+          <button className="button is-primary">+1</button> 
+          <button className="button is-danger">+2</button>
+          <button className="button is-info">+3</button>
+        </div>
+        <br/>
+        <div>
+          <button className="button is-success">-1</button>
+          <button className="button is-warning">-2</button>
+          <button className="button is-danger">-3</button>
+        </div>
       </header>
     </div>
   );
 
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    message: "This is message from mapStateToProps",
+    counter: state.counters || 'fuck you'
+  }
+}
+
+const AppWithConnect = connect(mapStateToProps)(App);
+
+export default AppWithConnect;
